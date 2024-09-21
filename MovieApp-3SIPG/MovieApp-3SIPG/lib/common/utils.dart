a91244b55eb1c237e47d969a15f3d5b896c272c7
Renato Takaoka <renato.takaoka13@gmail.com>
2024-09-21 17:59:05 -0300

    Adicionado funcao de detail e review

--- MovieApp-3SIPG/MovieApp-3SIPG/lib/common/utils.dart
+++ MovieApp-3SIPG/MovieApp-3SIPG/lib/common/utils.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 const imageUrl = 'https://image.tmdb.org/t/p/w500';
 const kBackgoundColor = Color(0xff121012);
 const kbuttonColor = Color.fromARGB(255, 89, 54, 133);
-const apiKey = '306b10d8a8f5f8f5efda8fd993d87be4';
+const apiKey = '';
 Color backgroundPrimary = Color.fromARGB(255, 171, 169, 180);
